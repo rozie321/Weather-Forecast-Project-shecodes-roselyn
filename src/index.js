1bb3a3da38c7ddcb790637940c5733f3eeb2a472
rozie321 <107440478+rozie321@users.noreply.github.com>
2024-06-04 12:33:22 +0100

    Integrating forecast api with function getForecast

--- src/index.js
+++ src/index.js
@@ -36,12 +36,43 @@ function HandleSearchSubmit(event){
 
 }
 
+function getForecast(city){
+    apiKey = "f0edft08a334d1a9c4eb5o0155c624af";
+    apiUrl= `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayForecast);
+}
 
 
+function displayForecast(response){
+
+    let days = ['Tue','Wed','Thur','Fri','Sat'];
+    let forecastHtml=" ";
+
+    days.forEach (function (day) {
+        forecastHtml=
+        forecastHtml +
+        `
+      <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-icon">🌤️</div>
+        <div class="weather-forecast-temperatures">
+          <div class="weather-forecast-temperature">
+            <strong>15º</strong>
+          </div>
+          <div class="weather-forecast-temperature">9º</div>
+        </div>
+      </div>
+        `;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
 let searchFormElement= document.querySelector("#search-form");
 searchFormElement.addEventListener("submit",HandleSearchSubmit);
 
 searchCity("Paris");
+getForecast("Paris");
 
 function formatDate(date) {
     let minutes = date.getMinutes();
